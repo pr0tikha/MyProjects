86c3f3a064b7e2f33e336fa1a2000f081c4e642d
Sujoy Singha <sujoykumar.singha@gmail.com>
2025-12-28 15:52:48 +0000

    calendar is appearing as list instead of a calendar like view. see the s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "react-day-picker/dist/style.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import { FirebaseClientProvider } from "@/firebase";
