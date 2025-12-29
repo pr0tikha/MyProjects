e682dc46d9aec875e0279c48b21c275209cd9c10
Sujoy Singha <sujoykumar.singha@gmail.com>
2025-12-29 19:44:20 +0000

    studio-1298148667:~/studio{Master-Branch}$ npx cap add ios [error] Could

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourcompany.paymind',
+  appName: 'PayMind',
+  webDir: 'out'
+};
+
+export default config;
